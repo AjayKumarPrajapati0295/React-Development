81c204a1279bd1cf2ad1e305ab6c38cf93ff74f7
AjayKumarPrajapati0295 <123166538+AjayKumarPrajapati0295@users.noreply.github.com>
2023-01-20 17:00:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="container">
+        <div class="subleft">
+            <div class="logo"><img src="Bearish 150 x 150.png"/></div>
+            <div class="content">Bearish OS</div>
+        </div>
+        <div class="subleft">
+            <div class="logo"><svg id="Light_1px_search" data-name="Light 1px/search" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                <rect id="Light_1px_search_background" data-name="Light 1px/search background" width="25" height="25" fill="#fff" opacity="0.003"/>
+                <path id="Combined_Shape" data-name="Combined Shape" d="M18.75,19.271a.518.518,0,0,1-.368-.153l-2.756-2.756a9.375,9.375,0,1,1,.737-.737l2.756,2.756a.521.521,0,0,1-.368.889ZM9.375,1.042a8.333,8.333,0,1,0,5.8,14.312.547.547,0,0,1,.173-.173A8.333,8.333,0,0,0,9.375,1.042Z" transform="translate(2.083 2.083)"/>
+              </svg></div>
+            <div class="content">Search Bearish OS</div>
+        </div>
+        <div class="subleft" id="dashboard">
+            <div class="logo"><svg id="Light_1px_home" data-name="Light 1px/home" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                <rect id="Light_1px_home_background" data-name="Light 1px/home background" width="25" height="25" fill="#fff" opacity="0"/>
+                <path id="Combined_Shape" data-name="Combined Shape" d="M17.708,20.8H3.125A3.128,3.128,0,0,1,0,17.671V9.687A3.129,3.129,0,0,1,1.034,7.365L8.326.8a3.125,3.125,0,0,1,4.181,0L19.8,7.365a3.129,3.129,0,0,1,1.034,2.322v7.984A3.128,3.128,0,0,1,17.708,20.8ZM9.375,10.379h2.083A3.129,3.129,0,0,1,14.584,13.5v6.25h3.125a2.085,2.085,0,0,0,2.083-2.083V9.687a2.082,2.082,0,0,0-.69-1.548L11.811,1.577a2.082,2.082,0,0,0-2.787,0L1.732,8.139a2.082,2.082,0,0,0-.69,1.548v7.984a2.085,2.085,0,0,0,2.083,2.083H6.25V13.5A3.128,3.128,0,0,1,9.375,10.379Zm0,1.042A2.085,2.085,0,0,0,7.292,13.5v6.25h6.249V13.5a2.085,2.085,0,0,0-2.083-2.083Z" transform="translate(2.083 2.121)"/>
+              </svg>
+              </div>
+            <div class="content">Dashboard</div>
+        </div>
+        <div class="subleft" id="videocall">
+            <div class="logo"><svg id="Light_1px_video_recorder" data-name="Light 1px/video_recorder" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                <rect id="Light_1px_video_recorder_background" data-name="Light 1px/video_recorder background" width="25" height="25" fill="#fff" opacity="0"/>
+                <path id="Combined_Shape" data-name="Combined Shape" d="M12.5,14.584H3.125A3.129,3.129,0,0,1,0,11.458V3.125A3.128,3.128,0,0,1,3.125,0H12.5a3.129,3.129,0,0,1,3.126,3.125V4.341l2.646-2.2a1.534,1.534,0,0,1,.992-.365,1.571,1.571,0,0,1,1.57,1.565v7.912a1.572,1.572,0,0,1-1.571,1.566,1.534,1.534,0,0,1-.991-.365l-2.646-2.2v1.215A3.129,3.129,0,0,1,12.5,14.584ZM3.125,1.042A2.085,2.085,0,0,0,1.042,3.125v8.333a2.085,2.085,0,0,0,2.083,2.083H12.5a2.086,2.086,0,0,0,2.084-2.083V3.125A2.086,2.086,0,0,0,12.5,1.042ZM19.269,2.814a.512.512,0,0,0-.331.122L15.625,5.7V8.887l3.312,2.76a.512.512,0,0,0,.331.121.534.534,0,0,0,.363-.144.513.513,0,0,0,.161-.377V3.336A.523.523,0,0,0,19.269,2.814Z" transform="translate(2.083 5.208)"/>
+              </svg>
+              </div>
+            <div class="content">Video Calls</div>
+            <div class="rightarrow"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                <g id="Light_1px_chevron_down" data-name="Light 1px/chevron_down" transform="translate(0 25) rotate(-90)">
+                  <rect id="Light_1px_chevron_down_background" data-name="Light 1px/chevron_down background" width="25" height="25" fill="#fff" opacity="0"/>
+                  <path id="Vector_474_Stroke_" data-name="Vector 474 (Stroke)" d="M.889.153,6.923,6.187a.521.521,0,0,0,.737,0L13.694.153a.521.521,0,0,1,.737.737L8.4,6.923a1.563,1.563,0,0,1-2.21,0L.153.889A.521.521,0,0,1,.889.153Z" transform="translate(5.208 9.115)"/>
+                </g>
+              </svg></div>
+        </div>
+        <div class="subleft" id="inbox">
+            <div class="logo"><svg id="Light_1px_mail" data-name="Light 1px/mail" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                <rect id="Light_1px_mail_background" data-name="Light 1px/mail background" width="25" height="25" fill="#fff" opacity="0"/>
+                <path id="Combined_Shape" data-name="Combined Shape" d="M17.708,16.667H3.125A3.129,3.129,0,0,1,0,13.541V3.125A3.128,3.128,0,0,1,3.125,0H17.708a3.128,3.128,0,0,1,3.125,3.125V13.541A3.129,3.129,0,0,1,17.708,16.667ZM1.042,3.592v9.95a2.086,2.086,0,0,0,2.083,2.084H17.708a2.086,2.086,0,0,0,2.083-2.084V3.592L11.887,9A2.6,2.6,0,0,1,8.947,9Zm2.083-2.55A2.09,2.09,0,0,0,1.166,2.414L9.534,8.14a1.563,1.563,0,0,0,1.764,0l8.369-5.726a2.09,2.09,0,0,0-1.959-1.372Z" transform="translate(2.083 4.167)"/>
+              </svg>
+              </div>
+            <div class="content">Inbox</div>
+            <div class="rightarrow"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                <g id="Light_1px_chevron_down" data-name="Light 1px/chevron_down" transform="translate(0 25) rotate(-90)">
+                  <rect id="Light_1px_chevron_down_background" data-name="Light 1px/chevron_down background" width="25" height="25" fill="#fff" opacity="0"/>
+                  <path id="Vector_474_Stroke_" data-name="Vector 474 (Stroke)" d="M.889.153,6.923,6.187a.521.521,0,0,0,.737,0L13.694.153a.521.521,0,0,1,.737.737L8.4,6.923a1.563,1.563,0,0,1-2.21,0L.153.889A.521.521,0,0,1,.889.153Z" transform="translate(5.208 9.115)"/>
+                </g>
+              </svg></div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
